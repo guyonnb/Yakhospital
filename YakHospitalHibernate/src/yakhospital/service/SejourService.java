8f994832dd6172d13e7316e59b5fa3171fddb56c
LOPVET Morgane <lopvet_m@epita.fr>
2012-05-24 20:30:10 +0200

    PatientService en cours

--- YakHospitalHibernate/src/yakhospital/service/SejourService.java
+++ YakHospitalHibernate/src/yakhospital/service/SejourService.java
@@ -4,10 +4,25 @@
  */
 package yakhospital.service;
 
+import java.util.Calendar;
+import yakhospital.hibernate.Patient;
+import yakhospital.hibernate.Sejour;
+import yakhospital.hibernate.Soin;
+
 /**
  *
  * @author Morgane
  */
 public class SejourService {
-    
+
+    private SejourService() {
+    }
+
+    static Sejour creerSejour(String raisonAdmission, boolean b, Calendar dateDebut, Patient p) {
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+
+    static void ajouterSoin(Sejour sejour, Soin soin) {
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
 }
