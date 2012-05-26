68b0745784108beab49a686d4a7cf584c8fcbd93
Mohamed Djenoual <djenou_m@epita.fr>
2012-05-26 14:35:56 +0200

    add objet pour test : main

--- YakHospitalHibernate/src/yakhospital/hibernate/Main.java
+++ YakHospitalHibernate/src/yakhospital/hibernate/Main.java
@@ -33,34 +33,68 @@ public class Main {
         Patient p8 = new Patient("Valentine", "Faye", "2951179532456");
         Patient p9 = new Patient("Osaki", "Nana", "2980699245597");
         Patient p10 = new Patient("Vika", "Barosse", "3992500245597");
+       
+        Poste posteAdmin = new Poste("Administrateur");
+        Poste posteMed = new Poste("Medecin");
+        Poste posteChir = new Poste("Chirurgien");
+        Poste posteAidSoi = new Poste("Aide soignant");
+        Poste posteSecret = new Poste("Secretaire");
+        Poste posteInterne = new Poste("Interne");
         
+        Service Pediatrie = new Service("Pediatrie");
+        Service Psychiatrie = new Service("Psychiatrie");
+        Service Neurologie = new Service("Neurologie");
+        Service cpre = new Service("cpre"); //Chirurgie plastique, reconstructrice et esthétique
+        Service Cancerologie = new Service("Cancerologie");
+        Service Cardiologie = new Service("Cardiologie");
+        Service Orthopedie = new Service("Orthopedie");
+        Service ar = new Service("ar");//Anesthésie et réanimation = salles de réveil
+                
+        Salle s1 = new Salle(15, "sallePédiatrie", Pediatrie);
+        Salle s2 = new Salle(2, "salleNeurologie", Neurologie);
+        Salle s3 = new Salle(8, "sallePsychiatrie", Psychiatrie);
+        Salle s4 = new Salle(4, "salleCPRE", cpre);
+        Salle s5 = new Salle(3, "salleCancerologie", Cancerologie);
+        Salle s6 = new Salle(5, "salleCardiologie", Cardiologie);
+        Salle s7 = new Salle(10, "salleOrthopedie", Orthopedie);
+        Salle s8 = new Salle(5, "salleAR", ar);
+                        
        
         Titulaire t1 = new Titulaire("Joel", "Yven","123");
         Titulaire t2 = new Titulaire("Hisaishi", "Joe","456");
         Titulaire t3 = new Titulaire("Amadeus", "Mozart","891");
         Titulaire t4 = new Titulaire("ludovicVan", "Beethoven","333");
         
-        TypeSoin ts1 = new TypeSoin("Cancérologie");
-        TypeSoin ts2 = new TypeSoin("Cardiologie");
-        TypeSoin ts3 = new TypeSoin("Psychiatrie");
-        TypeSoin ts4 = new TypeSoin("Pédiatrie");
-       
-        Sejour sejour1 = new Sejour("ulcère", date, p1);
-        Sejour sejour2 = new Sejour("ulcère", date, p1);
-        Sejour sejour3 = new Sejour("ulcère", date, p1);
-        Sejour sejour4 = new Sejour("ulcère", date, p1);
-        Sejour sejour5 = new Sejour("ulcère", date, p1);
-        Sejour sejour6 = new Sejour("ulcère", date, p1);
-        Sejour sejour7 = new Sejour("ulcère", date, p1);
-        Sejour sejour8 = new Sejour("ulcère", date, p1);
         
-        Soin soin1 = new Soin(date, "comment", ts1, t1);
-        sejour1.ajouterSoin(soin1);
+        TypeSoin ts1 = new TypeSoin("Coloscopie");
+        TypeSoin ts2 = new TypeSoin("intensifs");
+        TypeSoin ts3 = new TypeSoin("Consultation");
+        TypeSoin ts4 = new TypeSoin("Puericulture");
+       
+        Sejour sejour1 = new Sejour("ulcere", date, p1);
+        Sejour sejour2 = new Sejour("drepanocytose", date, p1);
+        Sejour sejour3 = new Sejour("supranucleaire", date, p1);
+        Sejour sejour4 = new Sejour("progéria", date, p1);
+        Sejour sejour5 = new Sejour("mucoviscidose", date, p1);
+        Sejour sejour6 = new Sejour("myopathie", date, p1);
+        Sejour sejour7 = new Sejour("myofasciite", date, p1);
+        Sejour sejour8 = new Sejour("leucodystrophie", date, p1);
+        
+        sejour1.ajouterSoin(new Soin(date, "comment", ts1, t1));
         sejour1.ajouterSoin(new Soin(date, "comment", ts2, t2));
         sejour2.ajouterSoin(new Soin(date, "comment", ts3, t3));
         sejour2.ajouterSoin(new Soin(date, "comment", ts4, t4));
-                        
-        p10.ajouterSejour(sejour1);
+                    
+        p1.ajouterSejour(sejour1);
+        p2.ajouterSejour(sejour1);
+        p3.ajouterSejour(sejour1);
+        p4.ajouterSejour(sejour1);
+        p5.ajouterSejour(sejour1);
+        p6.ajouterSejour(sejour2);
+        p7.ajouterSejour(sejour2);
+        p8.ajouterSejour(sejour2);
+        p9.ajouterSejour(sejour2); 
+        p10.ajouterSejour(sejour2);
         
         List<Patient> myList = new ArrayList<>();
         myList.add(p1);
