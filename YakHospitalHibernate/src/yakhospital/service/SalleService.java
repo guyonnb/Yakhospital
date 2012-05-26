041fa0293e6929ed0a7c81da63d5328573cf645d
Alexis Gimenez <alexis.gimenez.fr@gmail.com>
2012-05-26 16:30:15 +0200

    Modif de mes classes

--- YakHospitalHibernate/src/yakhospital/service/SalleService.java
+++ YakHospitalHibernate/src/yakhospital/service/SalleService.java
@@ -7,7 +7,9 @@ package yakhospital.service;
 import java.util.List;
 import java.util.Set;
 import yakhospital.hibernate.Salle;
+import yakhospital.hibernate.Sejour;
 import yakhospital.hibernate.Service;
+import yakhospital.hibernate.Soin;
 import yakhospital.hibernate.dao.impl.SalleDAOImpl;
 import yakhospital.hibernate.dao.impl.ServiceDAOImpl;
 
@@ -21,8 +23,13 @@ public class SalleService {
     }
     
     //voir si on set le service à la créationd de la salle ou plus tard (je pense plus tard)
-    public static Integer creerSalle(Integer nombreLits, String nomSalle) {
-        Salle salle = new Salle(nombreLits, nomSalle, null);
+    public static Integer creerSalle(Integer nombreLits, String nomSalle, Service service) {
+        Salle salle = new Salle(nombreLits, nomSalle, service);
+        return SalleDAOImpl.getInstance().save(salle);
+    }
+    
+    public static Integer creerSalle(Integer nombreLits, String nomSalle, Integer idService) {
+        Salle salle = new Salle(nombreLits, nomSalle, ServiceDAOImpl.getInstance().get(idService));
         return SalleDAOImpl.getInstance().save(salle);
     }
     
@@ -51,5 +58,11 @@ public class SalleService {
     
     public static List<Salle> consulterTouteSalle() {
         return SalleDAOImpl.getInstance().list();
+    }
+    
+    public static Boolean ajouterSoin(Salle salle, Soin soin) {
+        soin.setSalle(salle);
+        salle.ajouterSoin(soin);
+        return SalleDAOImpl.getInstance().update(salle);
     }
 }
